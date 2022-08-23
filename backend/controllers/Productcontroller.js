15ec8f73db111145aee2e02e74ab4ee374c70276
sharyar <sharyarmalik09@gmail.com>
2022-08-23 22:05:34 +0500

    module update

--- backend/controllers/Productcontroller.js
+++ backend/controllers/Productcontroller.js
@@ -1,3 +1,13 @@
+const Product = require("../model/productModule")
+
+
+exports.createProduct = async (res,req)=>{
+
+    const product = await Product.create(req.body);
+    res.status(201)
+
+}
+
 exports.getAllProducts = (req,res)=>{
     res.status(200).json({
         message:"route is working fine "
